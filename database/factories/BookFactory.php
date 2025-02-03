7c7ac5763623fc9a055a0070b480b9e5a5826c9a
SalavessaMota <nunosalavessa@hotmail.com>
2025-02-03 15:09:20 +0000

    First commit

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Publisher;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'isbn' => $this->faker->isbn13(),
+            'name' => $this->faker->words(3, true),
+            'publisher_id' => Publisher::factory(),
+            'description' => $this->faker->paragraph(),
+            'cover_image' => $this->faker->imageUrl(),
+            'price' => $this->faker->numberBetween(5, 1000),
+        ];
+    }
+}
